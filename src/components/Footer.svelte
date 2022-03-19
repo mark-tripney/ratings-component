e7303841d14f1c62756a38640fb40666079c68e5
Mark Tripney <marktripney@icloud.com>
2022-03-19 08:32:08 +0000

    Add footer (fixed at bottom)

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+  <p>Built by Mark Tripney; designed by Frontend Mentor.</p>
+</footer>
+
+<style>
+  footer {
+    color: var(--white);
+    margin: 0 auto;
+    margin-bottom: 1rem;
+  }
+</style>
