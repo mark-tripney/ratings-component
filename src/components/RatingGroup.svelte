0d6c26fa5179ce242b6cc6d2431af5b236df4fcd
Mark Tripney <marktripney@icloud.com>
2022-03-19 09:51:38 +0000

    Add RatingGroup component (group of circles)

---
+++ src/components/RatingGroup.svelte
@@ -0,0 +1,19 @@
+<script>
+  import RatingCircle from "./RatingCircle.svelte";
+</script>
+
+<div class="rating-group">
+  <RatingCircle rating="1" />
+  <RatingCircle rating="2" />
+  <RatingCircle rating="3" />
+  <RatingCircle rating="4" />
+  <RatingCircle rating="5" />
+</div>
+
+<style>
+  .rating-group {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1.5rem;
+  }
+</style>
