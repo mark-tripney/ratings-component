dd7ac62dea49da5a249035c60355a43cf76bb023
Mark Tripney <marktripney@icloud.com>
2022-03-19 08:37:09 +0000

    Add Ratings Card & Footer; remove Svelte defaults

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+  import RatingsCard from "./components/RatingsCard.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <RatingsCard />
 </main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 </style>
