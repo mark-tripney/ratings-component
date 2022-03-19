e66fd04f371d8aef74c76dfc9c3a1b321c602ae6
Mark Tripney <marktripney@icloud.com>
2022-03-19 09:55:48 +0000

    Add RatingCircle component, with hover

---
+++ src/components/RatingCircle.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let rating;
+</script>
+
+<div class="rating-circle">
+  <p>{rating}</p>
+</div>
+
+<style>
+  .rating-circle {
+    background-color: var(--darkBlue);
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+    display: grid;
+    place-items: center;
+    color: var(--mediumGrey);
+    cursor: pointer;
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    .rating-circle:hover {
+      background-color: var(--orange);
+      color: var(--white);
+    }
+  }
+</style>
