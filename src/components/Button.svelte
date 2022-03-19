b843f1c6ac69b6b27411dfe1edfdcbd446b1984c
Mark Tripney <marktripney@icloud.com>
2022-03-19 09:42:22 +0000

    Add Button component, with hover

---
+++ src/components/Button.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let buttonContent;
+</script>
+
+<button>
+  {buttonContent.toUpperCase()}
+</button>
+
+<style>
+  button {
+    background-color: var(--orange);
+    color: var(--white);
+    border: none;
+    font-size: 0.875rem;
+    letter-spacing: 2px;
+    width: 100%;
+    padding: 1em;
+    border-radius: 22px;
+    margin-bottom: 0.5rem;
+    cursor: pointer;
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    button:hover {
+      background-color: var(--white);
+      color: var(--orange);
+    }
+  }
+</style>
