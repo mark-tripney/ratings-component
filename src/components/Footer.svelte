3d47797f7ea9da73f010ce0b1cb8627ea98a92a9
Mark Tripney <marktripney@icloud.com>
2022-03-19 09:51:02 +0000

    Add Footer component, link to me and FE Mentor

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,12 @@
 <footer>
-  <p>Built by Mark Tripney; designed by Frontend Mentor.</p>
+  <p>
+    Built by <a href="https://marktripney.com" target="_blank">Mark Tripney</a>;
+    designed by
+    <a
+      href="https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI"
+      target="_blank">Frontend Mentor</a
+    >.
+  </p>
 </footer>
 
 <style>
@@ -7,5 +14,21 @@
     color: var(--white);
     margin: 0 auto;
     margin-bottom: 1rem;
+  }
+  p {
+    font-size: 0.8rem;
+    color: var(--white);
+  }
+  @media (min-width: 500px) {
+    p {
+      font-size: 0.9rem;
+    }
+  }
+  a,
+  a:visited {
+    color: var(--lightGrey);
+  }
+  a:hover {
+    color: var(--orange);
   }
 </style>
