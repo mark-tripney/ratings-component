1817bc3a4781333ddc01c40b2626cddd74b965b7
Mark Tripney <marktripney@icloud.com>
2022-03-19 08:31:51 +0000

    Add Ratings Card, incl. star svg

---
+++ src/components/RatingsCard.svelte
@@ -0,0 +1,38 @@
+<div class="card">
+  <div class="star-container">
+    <svg
+      class="svg-star"
+      width="17"
+      height="16"
+      xmlns="http://www.w3.org/2000/svg"
+      ><path
+        d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z"
+        fill="#FC7614"
+      /></svg
+    >
+  </div>
+</div>
+
+<style>
+  .card {
+    border-radius: 15px;
+    background-image: radial-gradient(
+      circle at 50% 0%,
+      #232a34 0%,
+      #181e27 99%
+    );
+    min-width: 312px;
+    height: 360px;
+    max-width: 412px;
+  }
+
+  .star-container {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: var(--darkBlue);
+    display: grid;
+    place-items: center;
+    margin: 1.5rem 0 0 1.5rem;
+  }
+</style>
